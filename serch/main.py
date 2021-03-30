abadbb8c047fd96be0019851bb0465c71be3d827
KonnaKotik <kotik.arina99@gmail.com>
2021-03-30 10:20:35 +0300

    убрали числительные

--- serch/main.py
+++ serch/main.py
@@ -3,14 +3,25 @@ import string
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 import pymorphy2
+import re
 
 
 def tokenize_search(file_text):
     tokens = word_tokenize(file_text, language="russian")
     tokens = [i for i in tokens if (i not in string.punctuation)]
+    tokens = [i.split('.')[0] for i in tokens]
+    tokens = [i.split(',')[0] for i in tokens]
+    tokens = [i.split('⋅')[0] for i in tokens]
+    tokens = [i.split('°')[0] for i in tokens]
+    tokens = [i for i in tokens if i.isdigit() == False]
     stop_words = stopwords.words('russian')
-    stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', 'к', 'на', 'комм.', 'а.е.', 'км', 'см'])
-    tokens = [i for i in tokens if (i not in stop_words)]
+    stop_words_eng = stopwords.words('english')
+    stop_words_eng.extend(['Y', 's', 'a', 'v', 'v1', 'v2', 't', 'k', 'A.', 'V', '°C', 'M☉', 'G', 'T', 'K', 'C', 'A'])
+    stop_words.extend(
+        ['что', 'это', 'так', 'вот', 'макс.', 'мин.', 'м/с²', 'км²', 'км³', 'км/ч', 'м/с', ' м/с', 'км/с', 'кг', 'г/см³', 'быть', 'как', 'в', '—',
+         'к', 'на', 'комм.', 'а.е.', 'км', 'см', 'см.', 'У', 'а', 'км/c', 'v', 'Mo', 'отн', 'i', 'сут', 'млрд', 'c', 'В',
+         'с', 'мин', 'ч', '«', '»', 'А'])
+    tokens = [i for i in tokens if (i not in stop_words and i not in stop_words_eng)]
     with open("token.txt", "a", encoding='UTF8') as token_file:
         for i in tokens:
             token_file.write(i + '\n')
@@ -19,31 +30,33 @@ def tokenize_search(file_text):
 
 if __name__ == '__main__':
     dict = {}
+    name = "1"
     for i in range(100):
         with open("files/выкачка_" + (i + 1).__str__() + ".txt", "r", encoding='UTF8') as file:
             content = file.read()
             tokenize_search(content)
-            #file_token = tokenize_search(content)
-            #print(file_token)
+            # file_token = tokenize_search(content)
+            # print(file_token)
     file_token = open("token.txt", "r", encoding='UTF8')
     morph = pymorphy2.MorphAnalyzer()
     lines = file_token.readlines()
     for j in lines:
-        #print(j)
+        # print(j)
         j = j.replace('\n', '')
-        lemma = morph.parse(j)[0].normal_form
+        if (morph.parse(j)[0].tag.POS != ('CONJ' or 'PREP' or 'PRCL' or 'INTJ' or 'ADVB' or 'ADVB' or 'PRED')) and (
+                j != ('еще' or 'ещё')):
+            lemma = morph.parse(j)[0].normal_form
         if dict.get(lemma) is None:
             dict[lemma] = [j]
         else:
             dict[lemma].append(j)
     lemma_file = open("lemma.txt", "a", encoding='UTF8')
     for k, v in dict.items():
-        #print(k, v)
+        # print(k, v)
         lemma_file.write(k)
         for a in v:
             lemma_file.write(" " + a)
         lemma_file.write("\n")
-
 
 #  with open("files/выкачка_1.txt", "r", encoding='UTF8') as file:
 # content = file.read(500)
